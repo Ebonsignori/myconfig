06ae3261634528a95beccb9fa660255268e6bfdb
Evan Bonsignori <evanabonsignori@gmail.com>
2021-09-21 13:20:32 -0700

    update aliases

--- dotfiles/zshrc/aliases.sh
+++ dotfiles/zshrc/aliases.sh
@@ -10,8 +10,11 @@ alias cd-myconfig='cd ~/Projects/myconfig'
 alias cd-pt='cd ~/Projects/pitch-trainer-midi'
 # github
 alias cd-booster='cd ~/Projects/github/how-we-work-booster-pack'
+alias cd-reports='cd ~/Projects/github/how-we-work-memex-reports-generator'
 alias cd-booster-testing='cd ~/Projects/github/how-we-work-booster-pack-testing'
+alias cd-reports-testing='cd ~/Projects/github/how-we-work-memex-reports-generator-testing'
 alias cd-booster-ebonsignori='cd ~/Projects/github/how-we-work-booster-pack-testing-ebonsignori'
+alias cd-reports-ebonsignori='cd ~/Projects/github/how-we-work-memex-reports-generator-testing-ebonsignori'
 
 # Git
 alias vdiff='git difftool'
